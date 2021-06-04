1fe32cb3811995d469dbde491530f3a25bfa575f
Denny Afrizal <denny.afrizal713@gmail.com>
2021-06-04 15:24:23 +0700

    Gantt Chart Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gantt-cart';
+    title = 'gantt-cart';
+    public gantt_ChartData = {
+        "date": "2017-02-09",
+        "taskArray": [
+            {
+                "task": "Breakfast",
+                "startTime": "8:30am",
+                "endTime": "11:00am"
+            },
+            {
+                "task": "Badge Pick-up",
+                "startTime": "8:00am",
+                "endTime": "7:00pm"
+            },
+            {
+                "task": "Angular 2",
+                "startTime": "9:00am",
+                "endTime": "8:00pm"
+            },
+            {
+                "task": "Lunch",
+                "startTime": "11:30am",
+                "endTime": "1:30pm"
+            },
+            {
+                "task": "Android Session",
+                "startTime": "8:30am",
+                "endTime": "8:00pm"
+            },
+            {
+                "task": "Codelabs",
+                "startTime": "8:30am",
+                "endTime": "8:00pm"
+            },
+            {
+                "task": "Closing Ceremony",
+                "startTime": "6:00pm",
+                "endTime": "10:00pm"
+            }
+        ]
+    }
+    public gantt_chart_options = {
+        rectColor: "red",
+        lineColor: "black",
+        labelColor: "green"
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {AppComponent} from "./app.component";
+// @ts-ignore
+import {GanttChart} from "angular-gantt-chart";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        GanttChart
+    ],
+    providers: [],
+    bootstrap: [
+        AppComponent
+    ]
 })
-export class AppModule { }
+
+export class AppModule {
+}
